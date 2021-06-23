8bfa8de8ea8ea67395263991a805e34db6941734
Sholto <madein.mania@gmail.com>
2021-06-23 12:59:22 +0100

    styling of main app component, import header

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,25 @@
 <script>
-	export let name;
+  import Header from './Header.svelte';
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<div class="container">
+  <div class="wrapper">
+    <Header />
+  </div>
+</div>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  .container {
+    display: flex;
+    flex-direction: column;
+    margin: auto;
+    min-height: 100%;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  .wrapper {
+    padding-bottom: 2em;
+    flex-grow: 1;
+    padding: 0 1em;
+    margin: auto;
+  }
 </style>
