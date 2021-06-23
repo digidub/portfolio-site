8767d62baf5b68fcc80ae96e0fd6f379016ff74e
Sholto <madein.mania@gmail.com>
2021-06-23 11:59:10 +0100

    page header

---
+++ src/Header.svelte
@@ -0,0 +1,10 @@
+<header class="page-header">
+  <a class="page-name" href="/">Alex Cox</a>
+  <nav class="page-nav">
+    <ul>
+      <li>Cv</li>
+      <li>Projects</li>
+      <li>Contact</li>
+    </ul>
+  </nav>
+</header>
