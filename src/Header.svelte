a4e2b694c7f4a746e18dff4c03dc068d45b64305
Sholto <madein.mania@gmail.com>
2021-06-23 12:44:41 +0100

    styling page header

--- src/Header.svelte
+++ src/Header.svelte
@@ -8,3 +8,38 @@
     </ul>
   </nav>
 </header>
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Lato&display=swap');
+
+  .page-header {
+    padding: 1em 0 0.5em 0;
+    border-bottom: 1px solid #379683;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .page-name {
+    font-family: 'Lato';
+    font-weight: 900;
+    text-align: center;
+    font-size: 3em;
+    text-decoration: none;
+    color: #edf5e1;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+
+  li {
+    display: inline;
+    padding: 0 0.25em;
+  }
+</style>
