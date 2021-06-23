f87c203e4673bc0d6e315ae708b7c40a1d81c83d
Sholto <madein.mania@gmail.com>
2021-06-23 13:01:20 +0100

    main homepage content

---
+++ src/Home.svelte
@@ -0,0 +1,26 @@
+<main>
+  <h1 class="page-intro">
+    I taught myself to build websites,
+    <br />such as this one.
+  </h1>
+  <div class="skills">
+    <section>
+      <h2>I'm a front-end web developer.</h2>
+      <p>HTML5, CSS3, JavaScript, React</p>
+    </section>
+    <section>
+      <h2>...and I also know how things work under the hood.</h2>
+      <p>NPM, Webpack, Git</p>
+    </section>
+    <section>
+      <h2>I'm enthused about learning more</h2>
+      <p>Node.js, Test-driven development</p>
+    </section>
+    <section>
+      <h2>And I'm ready to offer my skills</h2>
+      <p>Why not check out my <a href="/">portfolio</a>?</p>
+    </section>
+  </div>
+</main>
+
+<style></style>
