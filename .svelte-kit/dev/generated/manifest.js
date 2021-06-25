8a4be12184573b16377215d1c29c016be1e8505c
Sholto <madein.mania@gmail.com>
2021-06-25 14:37:25 +0100

    project restructure for sveltekit

--- .svelte-kit/dev/generated/manifest.js
+++ .svelte-kit/dev/generated/manifest.js
@@ -1,14 +1,22 @@
 const c = [
-	() => import("../components/layout.svelte"),
+	() => import("../../../src/routes/__layout.svelte"),
 	() => import("../components/error.svelte"),
-	() => import("../../../src/routes/index.svelte")
+	() => import("../../../src/routes/index.svelte"),
+	() => import("../../../src/routes/projects/index.svelte"),
+	() => import("../../../src/routes/contact/index.svelte")
 ];
 
 const d = decodeURIComponent;
 
 export const routes = [
 	// src/routes/index.svelte
-	[/^\/$/, [c[0], c[2]], [c[1]]]
+	[/^\/$/, [c[0], c[2]], [c[1]]],
+
+	// src/routes/projects/index.svelte
+	[/^\/projects\/?$/, [c[0], c[3]], [c[1]]],
+
+	// src/routes/contact/index.svelte
+	[/^\/contact\/?$/, [c[0], c[4]], [c[1]]]
 ];
 
 export const fallback = [c[0](), c[1]()];
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,57 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { fly } from 'svelte/transition';
+	import PageSlide from '../Components/PageSlide.svelte';
+</script>
+
+<PageSlide>
+	<main>
+		<h1 class="page-intro">
+			I taught myself to build websites,
+			<br />such as this one.
+		</h1>
+
+		<div class="skills">
+			<section>
+				<h2>I'm a front-end web developer.</h2>
+				<p>HTML5, CSS3, JavaScript, React, Svelte</p>
+			</section>
+			<section>
+				<h2>...and I also know how things work under the hood.</h2>
+				<p>NPM, Webpack, Git</p>
+			</section>
+			<section>
+				<h2>I'm enthused about learning more</h2>
+				<p>Node.js, Test-driven development</p>
+			</section>
+			<section>
+				<h2>And I'm ready to offer my skills</h2>
+				<p>Why not check out my <a href="/projects">portfolio</a>?</p>
+			</section>
+		</div>
+	</main>
+</PageSlide>
+
+<style>
+	.skills a {
+		color: #edf5e1;
+		font-weight: 600px;
+	}
+
+	.skills a:hover {
+		border-bottom: 1px solid #edf5e1;
+	}
+
+	@media only screen and (min-width: 65em) {
+		.page-intro {
+			margin: auto;
+			max-width: 500px;
+			margin-top: 1em;
+			margin-bottom: 1em;
+		}
+
+		.skills {
+			margin: auto;
+			max-width: 500px;
+		}
+	}
+</style>
