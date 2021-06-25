d99fbece5216464644cb08601462a03e2e6841e3
Sholto <madein.mania@gmail.com>
2021-06-25 11:18:26 +0100

    text

--- src/Projects.svelte
+++ src/Projects.svelte
@@ -1,8 +1,10 @@
 <script>
   import { fly } from 'svelte/transition';
-  import PageSlide from '../components/PageSlide.svelte';
+  import PageSlide from './Components/PageSlide.svelte';
+  import Card from './Components/ProjectCard.svelte';
 </script>
 
 <PageSlide>
+  <Card />
   <p>Projects</p>
 </PageSlide>
