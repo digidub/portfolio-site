a0a1e7e906bdf4eff317626ea3fde0ca0b24c350
Sholto <madein.mania@gmail.com>
2021-06-25 11:44:22 +0100

    project card template

---
+++ src/Components/ProjectCard.svelte
@@ -0,0 +1,34 @@
+<script>
+  //   import ghLogo from '/assets/GitHub.png';
+</script>
+
+<div class="card">
+  <div class="card-header">
+    <h1 class="card-title">Shopping-cart</h1>
+  </div>
+  <div class="card-body"><p>project description</p></div>
+  <div class="card-footer"><a href="https://github.com/digidub/shopping-cart2"><img src="/assets/GitHub.png" alt="GH logo" /></a></div>
+</div>
+
+<style>
+  .card {
+    height: 300px;
+    width: 400px;
+    border-radius: 15px;
+    background-color: #edf5e1;
+  }
+  .card-header {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    border-top-left-radius: 15px;
+    border-top-right-radius: 15px;
+    height: 50%;
+    background-image: url('https://i.imgur.com/JhXEWBq.png');
+    background-size: contain;
+  }
+  .card-title {
+    color: black;
+    margin: 0.1em;
+  }
+</style>
