adb9da119d202631995eb0cc5c97c883d469a0d7
Sholto <madein.mania@gmail.com>
2021-06-30 11:21:22 +0100

    dynamic props and styling for project card

--- src/Components/ProjectCard.svelte
+++ src/Components/ProjectCard.svelte
@@ -1,41 +1,79 @@
 <script>
-  //   import ghLogo from '/assets/GitHub.png';
+	export let name;
+	export let image;
+	export let technologies;
+	export let description;
+	export let liveLink;
+	export let repoLink;
 </script>
 
 <div class="card">
-  <div class="card-header">
-    <h1 class="card-title">Shopping-cart</h1>
-  </div>
-  <div class="card-body">
-    <p class="description">
-      A responsive shopping-cart built using React and styled components. Read the full writeup on GitHub, and check the project out!
-    </p>
-  </div>
-  <div class="card-footer"><a href="https://github.com/digidub/shopping-cart2"><img src="/assets/GitHub.png" alt="GH logo" /></a></div>
+	<div class="card-header">
+		<a href={repoLink}> <h2 class="card-title">{name}</h2></a>
+		<a href={repoLink}><img src="../../static/GitHub.png" alt="github logo" /></a>
+	</div>
+	<img src={image} alt="project" />
+	<div class="card-body">
+		<p class="technologies">
+			{technologies}
+		</p>
+		<p class="description">
+			{description}
+		</p>
+	</div>
+	<div class="card-footer">
+		<a href={liveLink}>Live</a>
+	</div>
 </div>
 
 <style>
-  .card {
-    height: 300px;
-    width: 400px;
-    border-radius: 15px;
-    background-color: #edf5e1;
-  }
-  .card-header {
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-end;
-    border-top-left-radius: 15px;
-    border-top-right-radius: 15px;
-    height: 50%;
-    background-image: url('https://i.imgur.com/JhXEWBq.png');
-    background-size: contain;
-  }
-  .card-title {
-    color: #05386b;
-    margin: 0.1em;
-  }
-  .description {
-    padding: 0.2em;
-  }
+	.card img {
+		max-width: 100%;
+	}
+
+	.card-header {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		align-items: flex-end;
+		margin-bottom: 10px;
+		border-bottom: 1px solid #8ee4af;
+	}
+
+	.card-header a {
+		text-decoration: none;
+		color: #05386b;
+	}
+
+	.card-header:hover {
+		border-bottom: 1px solid #05386b;
+	}
+
+	h2 {
+		margin-bottom: 0px;
+	}
+
+	/* .card {
+		height: 300px;
+		width: 400px;
+		border-radius: 15px;
+		background-color: #edf5e1;
+	}
+	.card-header {
+		display: flex;
+		flex-direction: column;
+		justify-content: flex-end;
+		border-top-left-radius: 15px;
+		border-top-right-radius: 15px;
+		height: 50%;
+		background-image: url('https://i.imgur.com/JhXEWBq.png');
+		background-size: contain;
+	}
+	.card-title {
+		color: #05386b;
+		margin: 0.1em;
+	}
+	.description {
+		padding: 0.2em;
+	} */
 </style>
