b51f45f384e854280e51138d8b633978864dea1a
Sholto <madein.mania@gmail.com>
2021-06-30 11:46:39 +0100

    rendering of project data into project cards

--- src/routes/projects/index.svelte
+++ src/routes/projects/index.svelte
@@ -1,14 +1,27 @@
 <script>
-	import { fly } from 'svelte/transition';
 	import PageSlide from '../../Components/PageSlide.svelte';
+	import ProjectCard from '../../Components/ProjectCard.svelte';
 	import Card from '../../Components/ProjectCard.svelte';
+	// import fetchData from '../../lib/fetchData';
+
+	async function fetchData() {
+		const response = await fetch('../../static/projects.json');
+		const data = await response.json();
+		return await data;
+	}
 </script>
 
 <PageSlide>
 	<main>
 		<h1 class="page-intro">Projects</h1>
 		<div class="projects">
-			<Card />
+			{#await fetchData()}
+				<pre>waiting</pre>
+			{:then data}
+				{#each data.projects as project}
+					<ProjectCard {...project} />
+				{/each}
+			{/await}
 		</div>
 	</main>
 </PageSlide>
