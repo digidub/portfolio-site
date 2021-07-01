121572ec9c78effbe69caabc5ddb9f73e1831781
Sholto <madein.mania@gmail.com>
2021-07-01 11:30:29 +0100

    projects intro text

--- src/routes/projects/index.svelte
+++ src/routes/projects/index.svelte
@@ -14,7 +14,12 @@
 <PageSlide>
 	<main>
 		<h1 class="page-intro">Projects</h1>
+
 		<div class="projects">
+			<h3 class="blurb">
+				Here are some projects that I've recently been working on. I'm always building something so
+				why not follow my <a href="https://github.com/digidub">GitHub</a> to stay up to date?
+			</h3>
 			{#await fetchData()}
 				<pre>waiting</pre>
 			{:then data}
@@ -32,12 +37,26 @@
 			margin: auto;
 			max-width: 500px;
 			margin-top: 1em;
-			margin-bottom: 1em;
+			margin-bottom: 0.5em;
 		}
 
 		.projects {
 			margin: auto;
 			max-width: 500px;
+		}
+
+		.blurb {
+			margin-top: 0px;
+		}
+
+		.projects a {
+			color: #edf5e1;
+			font-weight: 600px;
+			text-decoration: none;
+		}
+
+		.projects a:hover {
+			border-bottom: 1px solid #edf5e1;
 		}
 	}
 </style>
