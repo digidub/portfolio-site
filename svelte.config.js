3094fd584305130d4f8c81978eb3b8a1ca7a4c29
Sholto <madein.mania@gmail.com>
2021-07-02 07:56:01 +0100

    build configs

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,13 @@
 /** @type {import('@sveltejs/kit').Config} */
 import adapter from '@sveltejs/adapter-static';
-//import adapter from '@sveltejs/adapter-netlify';
 
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
 		adapter: adapter({
-			// default options are shown
-			pages: '/personal-portfolio',
-			assets: '/personal-portfolio',
+			pages: 'build',
+			assets: 'build',
 			fallback: 'index.html'
 		})
 	}
