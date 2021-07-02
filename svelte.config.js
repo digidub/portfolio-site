729cc39f68b74730a1b955bc6c450491f8ac5cb5
Sholto <madein.mania@gmail.com>
2021-07-02 07:58:01 +0100

    netlify adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,10 @@
 /** @type {import('@sveltejs/kit').Config} */
-import adapter from '@sveltejs/adapter-static';
-
+import adapter from '@sveltejs/adapter-netlify';
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html'
-		})
+		adapter: adapter()
 	}
 };
 
