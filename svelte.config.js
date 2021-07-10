0cae73ace040a276d2ec07836b7ad337449128c0
Sholto <madein.mania@gmail.com>
2021-07-10 08:55:34 +0100

    static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,9 @@
 /** @type {import('@sveltejs/kit').Config} */
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-static';
 const config = {
 	kit: {
 		target: '#svelte',
-		adapter: adapter(),
-		prerender: {
-			crawl: true,
-			enabled: true,
-			force: true,
-			pages: ['*']
-		}
+		adapter: adapter()
 	}
 };
 
