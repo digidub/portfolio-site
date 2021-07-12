f19c6352e53cf712a937180ffb657a60184f763d
Sholto <madein.mania@gmail.com>
2021-07-12 13:15:54 +0100

    vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,11 @@
 /** @type {import('@sveltejs/kit').Config} */
 import adapter from '@sveltejs/adapter-netlify';
+import vercel from '@sveltejs/adapter-vercel';
+
 const config = {
 	kit: {
 		target: '#svelte',
-		adapter: adapter(),
-		trailingSlash: 'always'
+		adapter: vercel()
 	}
 };
 
